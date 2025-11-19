5197a1932d30c295bd12abdb3528b9cdfb420561
alexismaras <alexis.maras@live.de>
2025-11-19 10:22:55 +0100

    Painting Level Entry Prototype
    Checks Player Prefs for Unlocked Level and changes appearence and entry behaviour of the painting.

---
+++ UnityProject/Assets/Scripts/PaintingLevelEntry.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PaintingLevelEntry : MonoBehaviour
+{
+    [SerializeField] private int _thisLevel;
+    private bool _canEnter = false;
+    private bool _isCompleted = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int levelUnlocked = PlayerPrefs.GetInt("LevelUnlocked", 1);
+        if (levelUnlocked == _thisLevel)
+        {
+            _canEnter = true;
+        }
+
+        if (levelUnlocked > _thisLevel)
+        {
+            _isCompleted = true;
+        }
+
+        if (_isCompleted)
+        {
+            // Change Appearance 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (_canEnter)
+        {
+            Debug.Log(collision.ToString());
+            // Load Scene
+        }
+
+    }
+}
