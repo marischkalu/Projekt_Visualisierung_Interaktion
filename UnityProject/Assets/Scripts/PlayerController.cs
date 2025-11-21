d934e43d8dc94d30a2181d6a6302018bc140eff0
alexismaras <alexis.maras@live.de>
2025-11-21 09:25:49 +0100

    Throwing "Orbs" + Camera Tilt Fix
    Added ThrowOrb() Function:
    - Spwans Sphere in front of camera
    - Adds Rigidbody to Sphere
    - Adds Force in view direction
    
    Clamped camera pitch value (-80 degrees <= pitch <= 80 degrees)

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     private float _verticalMouse;
     private float _horizontalInput;
     private float _verticalInput;
-    private float xRotation;
+    private float _pitch = 0f;
     [SerializeField] private float _rotationSpeedVertical = 500f;
     [SerializeField] private float _rotationSpeedHorizontal = 750f;
     [SerializeField] private float _movementSpeed = 0.2f;
+    [SerializeField] private float _throwForce = 200f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,32 +29,62 @@ public class PlayerController : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal"); // A/D
         _verticalInput = Input.GetAxis("Vertical");   // W/S
 
-        MovePlayerInLookDirection();
+        RotatePlayer();
+        MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ThrowOrb();
+        }
 
     }
 
 
-    void MovePlayerInLookDirection()
+    void RotatePlayer()
     {
 
-        Quaternion deltaRotation = Quaternion.Euler(0f, _horizontalMouse * _rotationSpeedHorizontal * Time.deltaTime, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(
+            0f,
+            _horizontalMouse * _rotationSpeedHorizontal * Time.deltaTime,
+            0f
+        );
         _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
 
-        if (_camera.transform.rotation.x > -0.7f && _verticalMouse <= 0)
-        {
-            _camera.transform.Rotate(_verticalMouse * _rotationSpeedVertical * Time.deltaTime * -1f, 0f, 0f);
-        }
-        if (_camera.transform.rotation.x < 0.7f && _verticalMouse > 0)
-        {
-            _camera.transform.Rotate(_verticalMouse * _rotationSpeedVertical * Time.deltaTime * -1f, 0f, 0f);
-        }
+        float inputPitch = _verticalMouse * _rotationSpeedVertical * Time.deltaTime * -1f;
+        _pitch += inputPitch;
 
+        _pitch = Mathf.Clamp(_pitch, -80f, 80f);
+        _camera.transform.localRotation = Quaternion.Euler(
+            _pitch,
+            0f,
+            0f
+        );
 
+    }
+
+    void MovePlayer()
+    {
         _rigidbody.MovePosition(
             _rigidbody.transform.position +
             _rigidbody.transform.forward * _verticalInput * _movementSpeed +
             _rigidbody.transform.right * _horizontalInput * _movementSpeed
         );
+    }
+
+    void ThrowOrb()
+    {
+        GameObject orb = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        orb.transform.position = _camera.transform.position + _camera.transform.forward.normalized * 2;
+        orb.transform.localScale = new Vector3(
+            0.5f,
+            0.5f,
+            0.5f
+        );
+
+        // Add a standard Rigidbody
+        Rigidbody rb = orb.AddComponent<Rigidbody>();
+
+        rb.AddForce(_camera.transform.forward * _throwForce);
     }
 
 }
