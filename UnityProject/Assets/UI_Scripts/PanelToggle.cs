6fba87e0c57b4c85d6aa590762a949d44fa79088
alexismaras <alexis.maras@live.de>
2026-02-06 16:42:22 +0100

    Fixed HUD Hint Toggle

--- UnityProject/Assets/UI_Scripts/PanelToggle.cs
+++ UnityProject/Assets/UI_Scripts/PanelToggle.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PanelToggle : MonoBehaviour
 {
     [SerializeField] private GameObject _panel;
     [SerializeField] private KeyCode _togglePanelKey = KeyCode.E;
 
+    void Start()
+    {
+        _panel.SetActive(false);
+    }
     void Update()
     {
         if (Input.GetKeyDown(_togglePanelKey))
         {
-            TogglePanel();
+            TogglePanels();
         }
     }
 
-    void TogglePanel()
+    void TogglePanels()
     {
         _panel.SetActive(!_panel.activeSelf);
     }
